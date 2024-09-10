5c60ea6e582b2f970fb4dd24bcce42535b08aa55
devPanzer <Abdulrahman.banaser@gmail.com>
2024-09-10 21:27:46 +0300

    Update Hello.java

--- src/main/java/com/example/project/Hello.java
+++ src/main/java/com/example/project/Hello.java
@@ -3,7 +3,7 @@ package com.example.project;
 public class Hello {
 
 	public static void main(final String[] args) {
-		Sysetem.out.println("Hello world!");
+		Sysetem.out.println("Hello world!\n");
 	}
 
 }
